613037ef9440a75edcfcd20e8100fef559c09d2d
zxlfly <zxlfly@foxmail.com>
2021-12-16 06:25:45 +0800

    fix: 修复没有自动生成log问题

--- versionrc.js
+++ versionrc.js
@@ -1,17 +1,16 @@
 module.exports = {
     "types": [
-      { "type": "feat", "section": "✨ Features | 新功能" },
-      { "type": "fix", "section": "🐛 Bug Fixes | Bug 修复" },
-      { "type": "init", "section": "🎉 Init | 初始化" },
-      { "type": "docs", "section": "✏️ Documentation | 文档" },
-      { "type": "style", "section": "💄 Styles | 风格" },
-      { "type": "refactor", "section": "♻️ Code Refactoring | 代码重构" },
-      { "type": "perf", "section": "⚡ Performance Improvements | 性能优化" },
-      { "type": "test", "section": "✅ Tests | 测试" },
-      { "type": "revert", "section": "⏪ Revert | 回退" },
-      { "type": "build", "section": "📦‍ Build System | 打包构建" },
-      { "type": "chore", "section": "🚀 Chore | 构建/工程依赖/工具" },
-      { "type": "ci", "section": "👷 Continuous Integration | CI 配置" }
+        { "type": "chore", "section": "Others", },
+        { "type": "revert", "section": "Reverts", },
+        { "type": "feat", "section": "Features", },
+        { "type": "fix", "section": "Bug Fixes", },
+        { "type": "improvement", "section": "Feature Improvements", },
+        { "type": "docs", "section": "Docs", },
+        { "type": "style", "section": "Styling", },
+        { "type": "refactor", "section": "Code Refactoring", },
+        { "type": "perf", "section": "Performance Improvements", },
+        { "type": "test", "section": "Tests", },
+        { "type": "build", "section": "Build System", },
+        { "type": "ci", "section": "CI", }
     ]
-  }
-  
+}
